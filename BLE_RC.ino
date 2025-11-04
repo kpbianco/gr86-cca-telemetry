25edd47c20b55737491afb7878eff3ae9af3316b
Kian Bianco <52121367+kpbianco@users.noreply.github.com>
2025-11-03 18:33:36 -0700

    Update BLE_RC.ino

--- BLE_RC.ino
+++ BLE_RC.ino
@@ -39,7 +39,7 @@
 #endif
 #endif
 #include "config.h"   // must define DEFAULT_UPDATE_RATE_DIVIDER and DEVICE_NAME
-#include <gps_nmea.h>
+#include "src/gps_nmea.h"
 
 // ---------- Version ----------
 #ifndef FW_VERSION
